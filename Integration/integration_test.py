369058d80ada2c08aedde1db69db2386b669c088
Minaz Shaikh <mshaikh1415@conestogac.on.ca>
2023-09-28 00:00:41 -0400

    Division and multiplication changes

--- Integration/integration_test.py
+++ Integration/integration_test.py
@@ -15,10 +15,10 @@ def test_subtraction():
     assert calculator.subtract(5, 3) == 2
 
 def test_multiplication():
-    assert calculator.multiply(2, 3) == 6
+    assert calculator.multiply(2, 2) == 6
 
 def test_division():
-    assert calculator.divide(6, 3) == 2
+    assert calculator.divide(6, 3) == 0
 
 def test_divide_by_zero():
     try:
