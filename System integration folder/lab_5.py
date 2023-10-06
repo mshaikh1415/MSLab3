ab0e6f0001431c3edfd13f34e024f0dcb2261fc0
Minaz Shaikh <mshaikh1415@conestogac.on.ca>
2023-10-06 19:25:56 -0400

    Changes done to selenium file

--- System integration folder/lab_5.py
+++ System integration folder/lab_5.py
@@ -22,36 +22,36 @@ search_bar.send_keys("laptop")
 search_bar.send_keys(Keys.RETURN)
 
 # Waiting for the search results page to load
-time.sleep(5)
+time.sleep(4)
 
 # Verifying that the search results page has loaded
 assert "laptop" in driver.title
 
 # Selecting a laptop from the search results
-laptop_link = driver.find_element("xpath","/html/body/div[1]/div[2]/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/div/div/div[1]/span/a/div/img")
+laptop_link = driver.find_element("xpath","/html/body/div[1]/div[1]/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/div/div/div[2]/div[1]/h2/a")
 # laptop_link = driver.find_element("By.CSS_SELECTOR","span[data-component-type='s-product-image'] a")
 laptop_link.click()
 
 
 
 # Waiting for the laptop details page to load
-time.sleep(5)
+time.sleep(4)
 
 # Adding the laptop to the cart
 add_to_cart_button = driver.find_element("id","add-to-cart-button")
 add_to_cart_button.click()
 
 # Waiting for the cart to update
-time.sleep(5)
+time.sleep(4)
 
 # Clicking on no thanks button
-no_thanks_button= driver.find_element("xpath","/html/body/div[9]/div[3]/div[1]/div/div/div[2]/div[2]/div/div/div[3]/div/span[2]/span/input")
+no_thanks_button= driver.find_element("xpath","/html/body/div[8]/div[3]/div[1]/div/div/div[2]/div[2]/div/div/div[3]/div/span[2]/span/input")
 no_thanks_button.click()
-time.sleep(2)
+time.sleep(4)
 
-proceed_to_checkout= driver.find_element("xpath","/html/body/div[1]/div[2]/div/div[1]/div[2]/div/div[3]/div/div[1]/form/span/span/span/input")
+proceed_to_checkout= driver.find_element("xpath","/html/body/div[1]/div[1]/div/div[1]/div[2]/div/div[1]/form/span/span/span/input")
 proceed_to_checkout.click()
-time.sleep(2)
+time.sleep(4)
 
 
 # Verifying that the laptop has been added to the cart
